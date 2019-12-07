8da2a7282ed68c4c195d79d323ebbc69eeb5a4dd
hayata-yamamoto <hayata.pri.yamamoto@gmail.com>
2019-12-08 00:46:10 +0900

    formatted

--- examples/sklearn.py
+++ examples/sklearn.py
@@ -1,7 +1,8 @@
 import numpy as np
 from flair import embeddings
 from sklearn import datasets, neural_network, metrics
-from tqdm import tqdm 
+from tqdm import tqdm
+
 
 def embed_by_model(s: str, e: embeddings.DocumentEmbeddings) -> np.ndarray:
     sent = embeddings.Sentence(s)
@@ -10,16 +11,22 @@ def embed_by_model(s: str, e: embeddings.DocumentEmbeddings) -> np.ndarray:
 
 
 def main() -> None:
-    x_train, y_train = datasets.fetch_20newsgroups(subset='train', return_X_y=True)
-    x_test, y_test = datasets.fetch_20newsgroups(subset='test', return_X_y=True)
-    
-    doc_embeddings = embeddings.DocumentPoolEmbeddings([
-        embeddings.WordEmbeddings('glove')
-        ])
-    x_train = np.array([embed_by_model(x, doc_embeddings) for x in tqdm(x_train)])
-    x_test = np.array([embed_by_model(x, doc_embeddings) for x in tqdm(x_test)])
+    x_train, y_train = datasets.fetch_20newsgroups(subset='train',
+                                                   return_X_y=True)
+    x_test, y_test = datasets.fetch_20newsgroups(subset='test',
+                                                 return_X_y=True)
 
-    clf = neural_network.MLPClassifier(hidden_layer_sizes=(100,), early_stopping=True, random_state=12345, verbose=True)
+    doc_embeddings = embeddings.DocumentPoolEmbeddings(
+        [embeddings.WordEmbeddings('glove')])
+    x_train = np.array(
+        [embed_by_model(x, doc_embeddings) for x in tqdm(x_train)])
+    x_test = np.array(
+        [embed_by_model(x, doc_embeddings) for x in tqdm(x_test)])
+
+    clf = neural_network.MLPClassifier(hidden_layer_sizes=(100, ),
+                                       early_stopping=True,
+                                       random_state=12345,
+                                       verbose=True)
     clf.fit(x_train, y_train)
 
     y_pred = clf.predict(x_test)
